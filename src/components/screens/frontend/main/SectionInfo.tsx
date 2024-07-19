ae9bb279c086314360124367cfa225526067791a
Alex Novak <alexleonnovak@gmail.com>
2024-07-19 20:25:35 +0300

    fixes for deploy

--- src/components/screens/frontend/main/SectionInfo.tsx
+++ src/components/screens/frontend/main/SectionInfo.tsx
@@ -9,9 +9,9 @@ export const SectionInfo = () => {
         <Image src="/memory-box-logo.svg" alt="Memory Box Logo" width={170} height={41} />
       </div>
       <Image src="/info.jpg" alt="Info" width={1300} height={655}/>
-      <p>"Zberezhemo" is a joint project by Memory Box and IAC ISHR. We analyzed the requests of people from the most vulnerable regions of Ukraine and realized that often they want to preserve their history and emotions hidden in old photos, videotapes, or children's drawings. Memories for them are a powerful source of strength and support. So we understood... we must help.
+      <p>&quot;Zberezhemo&quot; is a joint project by Memory Box and IAC ISHR. We analyzed the requests of people from the most vulnerable regions of Ukraine and realized that often they want to preserve their history and emotions hidden in old photos, videotapes, or children&apos;s drawings. Memories for them are a powerful source of strength and support. So we understood... we must help.
 
-        We offered people to digitize, revive, and preserve their family archives. And from the materials they wished to share, we created a library. There are no books, no shelves, or catalogs here, but, as in any other library, you can carefully take and respectfully explore the living memories. Here are the stories of those whom "Zberezhemo" has already helped, and new additions are constantly arriving.
+        We offered people to digitize, revive, and preserve their family archives. And from the materials they wished to share, we created a library. There are no books, no shelves, or catalogs here, but, as in any other library, you can carefully take and respectfully explore the living memories. Here are the stories of those whom &quot;Zberezhemo&quot; has already helped, and new additions are constantly arriving.
       </p>
     </section>
   )
