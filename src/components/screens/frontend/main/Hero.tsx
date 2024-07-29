7fe05258d88d23199334360b0e8bf2219dfe0e4f
Juzzzt <vlyahov@gmail.com>
2024-07-30 02:56:21 +0300

    fixes

--- src/components/screens/frontend/main/Hero.tsx
+++ src/components/screens/frontend/main/Hero.tsx
@@ -5,31 +5,31 @@ import './css/hero.css';
 import Image from 'next/image';
 import ScrollAnimation from 'react-animate-on-scroll';
 import 'animate.css/animate.compat.css';
+import { HeroBackAnimation } from './HeroBackAnimation';
+import { BrowserView, isMobile } from 'react-device-detect';
 
 export const Hero = () => {
     return (
         <section className="hero">
             <div className="wrapper">
-                <ScrollAnimation animateIn="fadeInDown">
-                    <div className="hero__title">MEMORY</div>
-                </ScrollAnimation>
-                <ScrollAnimation animateIn="fadeInDown">
-                    <div className="hero__sub-title">LIBRARY</div>
-                </ScrollAnimation>
-                <ScrollAnimation
-                    animateIn="fadeInDown"
-                    animateOut="fadeOutRight"
-                >
-                    <div className="hero__text">PEOPLE. CITIES. EVENTS</div>
-                </ScrollAnimation>
-
-                <Image
-                    src="/memory-bg.png"
-                    className="hero__bg"
-                    alt="MEMORY LIBRARY PEOPLE. CITIES. EVENTS"
-                    width={160}
-                    height={288}
-                />
+                <HeroBackAnimation></HeroBackAnimation>
+                <div className="hero-texts">
+                    <ScrollAnimation animateIn="fadeInDown">
+                        <div className="hero__title">MEMORY</div>
+                    </ScrollAnimation>
+                    <ScrollAnimation animateIn="fadeInDown">
+                        <div className="hero__sub-title">LIBRARY</div>
+                    </ScrollAnimation>
+                    <ScrollAnimation
+                        animateIn="fadeInDown"
+                        animateOut="fadeOutRight"
+                    >
+                        <div className="hero__text">PEOPLE. CITIES. EVENTS</div>
+                    </ScrollAnimation>
+                </div>
+                {/* <div className="hero__title">MEMORY</div>
+                <div className="hero__sub-title">LIBRARY</div>
+                <div className="hero__text">PEOPLE. CITIES. EVENTS</div> */}
             </div>
         </section>
     );
