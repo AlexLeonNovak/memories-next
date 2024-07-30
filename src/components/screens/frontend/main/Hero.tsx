22a013154bad31caa8dd7937abf22e019e53ef00
Juzzzt <vlyahov@gmail.com>
2024-07-31 02:44:57 +0300

    added aminations

--- src/components/screens/frontend/main/Hero.tsx
+++ src/components/screens/frontend/main/Hero.tsx
@@ -1,35 +1,135 @@
 'use client';
 
-import React from 'react';
+import React, { useRef } from 'react';
 import './css/hero.css';
 import Image from 'next/image';
 import ScrollAnimation from 'react-animate-on-scroll';
 import 'animate.css/animate.compat.css';
 import { HeroBackAnimation } from './HeroBackAnimation';
 import { BrowserView, isMobile } from 'react-device-detect';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
 export const Hero = () => {
+    const container = React.useRef<HTMLDivElement>(null);
+    useGSAP(
+        () => {
+            gsap.fromTo(
+                '.hero__title .hero-text-letter',
+                {
+                    'will-change': 'opacity',
+                    opacity: 0,
+                    filter: 'blur(5px)',
+                },
+                {
+                    duration: 1.75,
+                    ease: 'power1.inOut',
+                    opacity: 1,
+                    filter: 'blur(0px)',
+                    stagger: { each: 0.1, from: 'random' },
+                    // scrollTrigger: {
+                    //     trigger: title,
+                    //     start: 'top bottom',
+                    //     end: 'center center',
+                    //     toggleActions: "play resume resume reset"
+                    // }
+                }
+            );
+            gsap.fromTo(
+                '.hero__sub-title .hero-text-letter',
+                {
+                    'will-change': 'opacity',
+                    opacity: 0,
+                    filter: 'blur(5px)',
+                },
+                {
+                    duration: 1.75,
+                    ease: 'power1.inOut',
+                    opacity: 1,
+                    filter: 'blur(0px)',
+                    stagger: { each: 0.1, from: 'random' },
+                    // scrollTrigger: {
+                    //     trigger: title,
+                    //     start: 'top bottom',
+                    //     end: 'center center',
+                    //     toggleActions: "play resume resume reset"
+                    // }
+                }
+            );
+            gsap.fromTo(
+                '.hero__text .hero-text-letter',
+                {
+                    'will-change': 'opacity',
+                    opacity: 0,
+                    filter: 'blur(10px)',
+                },
+                {
+                    duration: 0.75,
+                    ease: 'power1.inOut',
+                    opacity: 1,
+                    filter: 'blur(0px)',
+                    stagger: { each: 0.02, from: 'random' },
+                    // scrollTrigger: {
+                    //     trigger: title,
+                    //     start: 'top bottom',
+                    //     end: 'center center',
+                    //     toggleActions: "play resume resume reset"
+                    // }
+                }
+            );
+        },
+        { scope: container }
+    );
+
+    const getSymbolItems = (text: string, className: string) => {
+        return text.split('').map((symbol, index) => {
+          const value = symbol !== ' ' ? symbol : <div>&nbsp;</div>;
+            return (
+                <div key={index} className={className}>
+                    {value}
+                </div>
+            );
+        });
+    };
+
+    const title = 'MEMORY';
+    const subtitle = 'LIBRARY';
+    const text = 'PEOPLE. CITIES. EVENTS.';
+    const className = 'hero-text-letter';
+
+    const titleItems = getSymbolItems(title, className);
+    const subtitleItems = getSymbolItems(subtitle, className);
+    const textItems = getSymbolItems(text, className);
+
     return (
         <section className="hero">
-            <div className="wrapper">
+            <div className="wrapper" ref={container}>
                 <HeroBackAnimation></HeroBackAnimation>
                 <div className="hero-texts">
-                    <ScrollAnimation animateIn="fadeInDown">
+                    <div className="hero__title">{titleItems}</div>
+                    <div className="hero__sub-title">{subtitleItems}</div>
+                    <div className="hero__text">{textItems}</div>
+                    {/* <ScrollAnimation animateIn="fadeInDown">
                         <div className="hero__title">MEMORY</div>
                     </ScrollAnimation>
                     <ScrollAnimation animateIn="fadeInDown">
                         <div className="hero__sub-title">LIBRARY</div>
-                    </ScrollAnimation>
-                    <ScrollAnimation
+                    </ScrollAnimation> */}
+                    {/* <ScrollAnimation
                         animateIn="fadeInDown"
                         animateOut="fadeOutRight"
                     >
                         <div className="hero__text">PEOPLE. CITIES. EVENTS</div>
-                    </ScrollAnimation>
+                    </ScrollAnimation> */}
                 </div>
+
                 {/* <div className="hero__title">MEMORY</div>
                 <div className="hero__sub-title">LIBRARY</div>
-                <div className="hero__text">PEOPLE. CITIES. EVENTS</div> */}
+                <div className="hero__text">PEOPLE. CITIES. EVENTS.</div> */}
             </div>
         </section>
     );
--- src/components/screens/frontend/main/SectionFeedback.tsx
+++ src/components/screens/frontend/main/SectionFeedback.tsx
@@ -1,14 +1,74 @@
 'use client';
 
+import React from 'react';
 import { FeedbackForm } from '@/components';
 import './css/feedback.css';
-import ScrollAnimation from 'react-animate-on-scroll';
-import 'animate.css/animate.compat.css';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(ScrollTrigger);
 
 export const SectionFeedback = () => {
+    const container = React.useRef<HTMLDivElement>(null);
+    useGSAP(
+        () => {
+            [
+                '.feedback__line--one .feedback-letter',
+                '.feedback__line--two .feedback-letter',
+                '.feedback__line--three .feedback-letter',
+            ].forEach((selector) => {
+                gsap.fromTo(
+                    selector,
+                    {
+                        'will-change': 'opacity',
+                        opacity: 0,
+                        filter: 'blur(5px)',
+                    },
+                    {
+                        duration: 0.75,
+                        ease: 'power1.inOut',
+                        opacity: 1,
+                        filter: 'blur(0px)',
+                        stagger: { each: 0.025, from: 'random' },
+                        scrollTrigger: {
+                            trigger: '.feedback__title',
+                            start: 'top bottom',
+                            end: 'center center',
+                            toggleActions: 'play resume resume reset',
+                        },
+                    }
+                );
+            });
+        },
+        { scope: container }
+    );
+
+    console.log('test feedback');
+    const getSymbolItems = (text: string, className: string) => {
+        return text.split('').map((symbol, index) => {
+            const value = symbol !== ' ' ? symbol : <div>&nbsp;</div>;
+            return (
+                <div key={index} className={className}>
+                    {value}
+                </div>
+            );
+        });
+    };
+
+    const lineOne = "LET'S SAVE";
+    const lineTwo = 'THE PAST FOR';
+    const lineThree = 'THE FUTURE.';
+    const className = 'feedback-letter';
+
+    const lineOneItems = getSymbolItems(lineOne, className);
+    const lineTwoItems = getSymbolItems(lineTwo, className);
+    const lineThreeItems = getSymbolItems(lineThree, className);
+
     return (
         <section id="feedback" className="feedback">
-            <div className="wrapper">
+            <div className="wrapper" ref={container}>
                 <div className="feedback__text">
                     <i>
                         “Family faces are magic mirrors. Looking at people who
@@ -16,13 +76,17 @@ export const SectionFeedback = () => {
                     </i>{' '}
                     Gail Lumet Buckley
                 </div>
-                <ScrollAnimation animateIn="fadeInDown">
-                    <div className="feedback__title">
-                        <span>LET’S SAVE</span>
-                        <span>THE PAST FOR</span>
-                        <span>THE FUTURE.</span>
+                <div className="feedback__title">
+                    <div className="feedback__line feedback__line--one">
+                        {lineOneItems}
                     </div>
-                </ScrollAnimation>
+                    <div className="feedback__line feedback__line--two">
+                        {lineTwoItems}
+                    </div>
+                    <div className="feedback__line feedback__line--three">
+                        {lineThreeItems}
+                    </div>
+                </div>
                 <FeedbackForm />
             </div>
         </section>
