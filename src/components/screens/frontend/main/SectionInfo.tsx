4d8ff0f49892597061dbf51d79644a4ed97279c4
Alex Novak <alexleonnovak@gmail.com>
2024-08-28 17:39:30 +0300

    add links to sponsors

--- src/components/screens/frontend/main/SectionInfo.tsx
+++ src/components/screens/frontend/main/SectionInfo.tsx
@@ -14,10 +14,18 @@ export const SectionInfo = () => {
         <div className='info-main'>
           <div className='info-main__text' dangerouslySetInnerHTML={{ __html: t.raw('[html]text') }} />
           <div className='info-main__sponsors'>
-            <a href='#' className='info-main__sponsors-item info-main__sponsors-item_logo'>
+            <a
+              href='https://iac-ishr.org/'
+              target='_blank'
+              className='info-main__sponsors-item info-main__sponsors-item_logo'
+            >
               <Image src='/ishr-iac-logo.svg' alt='ISHR Logo' width={106} height={45} />
             </a>
-            <a href='#' className='info-main__sponsors-item info-main__sponsors-item_memory'>
+            <a
+              href='https://mymemory-box.com/'
+              target='_blank'
+              className='info-main__sponsors-item info-main__sponsors-item_memory'
+            >
               <Image src='/memory-box-logo.svg' alt='Memory Box Logo' width={184} height={18} />
             </a>
           </div>
